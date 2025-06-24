f2a169b95542a5183fc5b0239b6c0bbf076a149a
Danila Danilov <danila.Zcode@gmail.com>
2025-06-24 16:57:49 +0300

    change main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -10,17 +10,14 @@ using namespace catalogue;
 using namespace renderer;
 
 int main() {
-    ifstream file("s10_final_opentest_3.json"s);
-
     TransportCatalogue catalogue;
     
-    json_reader::JsonReader reader(file);
-    reader.FillTransportCatalogue(catalogue);
+    json_reader::JsonReader reader(cin);
+    reader.ApplyBaseRequests(catalogue);
+
     MapRenderer renderer(reader.GetRenderSettings());
 
-
     RequestHandler handler(catalogue, renderer);
-
-    ofstream output("s10_final_opentest_3_my_answer.json"s);
-    reader.ApplyStatRequests(handler, output);
+    
+    reader.ApplyStatRequests(catalogue, handler, cout);
 }
